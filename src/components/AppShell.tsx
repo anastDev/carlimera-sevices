900ed910a95f82b1ce8449e3fa87faeaa41396ee
anastasiam <anast.moud@gmail.com>
2026-07-23 17:29:21 +0200

    feat: add again button and logic for BookingDialog

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,4 +1,4 @@
-import {type ReactNode, useLayoutEffect} from "react";
+import {type ReactNode, useLayoutEffect, useState} from "react";
 import {Route, Routes, useLocation} from "react-router";
 import HomePage from "@/pages/Homepage.tsx";
 import AboutPage from "@/pages/Aboutpage.tsx";
@@ -9,6 +9,9 @@ import {cars} from "@/data/carData.ts";
 import Header from "@/components/layout/header/Header.tsx";
 import InventoryPage from "@/pages/InventoryPage.tsx";
 import CarPage from "@/pages/CarPage.tsx";
+import {toast} from "sonner";
+import type {Car} from "@/types/typesCar.ts";
+import BookingDialog from "@/components/BookingDialog.tsx";
 
 interface WrapperProps {
     children: ReactNode;
@@ -25,19 +28,19 @@ function Wrapper({ children }: WrapperProps) {
 }
 
 export const  AppShell = ()=>  {
-    // const [bookingCar, setBookingCar] = useState<Car | null>(null);
-    // const [dialogOpen, setDialogOpen] = useState(false);
-    //
-    // const handleBookViewing = (car: Car) => {
-    //     setBookingCar(car);
-    //     setDialogOpen(true);
-    // };
-    //
-    // const handleConfirmBooking = ( dateId: string, timeId: string) => {
-    //     toast("Booking confirmed!",{
-    //         description: `${dateId} ${timeId}`,
-    //     });
-    // };
+    const [bookingCar, setBookingCar] = useState<Car | null>(null);
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+    const handleBookViewing = (car: Car) => {
+        setBookingCar(car);
+        setDialogOpen(true);
+    };
+
+    const handleConfirmBooking = ( dateId: string, timeId: string) => {
+        toast("Booking confirmed!",{
+            description: `${dateId} ${timeId}`,
+        });
+    };
 
     return (
         <Wrapper>
@@ -45,26 +48,26 @@ export const  AppShell = ()=>  {
 
             <main className="container mx-auto max-w-7xl px-4 pb-16 pt-10 sm:px-6 lg:px-8">
                 <Routes>
-                    <Route path="/" element={<HomePage cars={cars}/>} />
+                    <Route path="/" element={<HomePage cars={cars} onBookViewing={handleBookViewing}/>} />
 
                     <Route path="about" element={<AboutPage />} />
                     <Route path="contact" element={<ContactPage />} />
 
                     <Route path="cars">
                         <Route index element={<InventoryPage cars={cars} />} />
-                        <Route path=":id" element={<CarPage cars={cars} />} />
+                        <Route path=":id" element={<CarPage cars={cars} onBookViewing={handleBookViewing}/>} />
                     </Route>
                 </Routes>
             </main>
 
             <Footer />
 
-            {/*<BookingDialog*/}
-            {/*    car={bookingCar}*/}
-            {/*    open={dialogOpen}*/}
-            {/*    onOpenChange={setDialogOpen}*/}
-            {/*    onConfirm={handleConfirmBooking}*/}
-            {/*/>*/}
+            <BookingDialog
+                car={bookingCar}
+                open={dialogOpen}
+                onOpenChange={setDialogOpen}
+                onConfirm={handleConfirmBooking}
+            />
         </Wrapper>
     );
 }
